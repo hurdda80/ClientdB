98c568b34d68eb6d713026ad219151126b40136b
Dean H <dean.hurd80@yahoo.com>
2023-12-26 12:15:35 -0600

    gettin started

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module dhurd.c195.clientdb {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens dhurd.c195.clientdb to javafx.fxml;
+    exports dhurd.c195.clientdb;
+    exports dhurd.c195.clientdb.controllers;
+    opens dhurd.c195.clientdb.controllers to javafx.fxml;
+}
