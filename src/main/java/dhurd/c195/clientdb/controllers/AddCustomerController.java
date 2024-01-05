c3197f93387c2924247e764b34b140e036f7ce45
Dean H <dean.hurd80@yahoo.com>
2024-01-05 11:48:24 -0600

    Finished add/update customer views and started the controllers

--- src/main/java/dhurd/c195/clientdb/controllers/AddCustomerController.java
+++ src/main/java/dhurd/c195/clientdb/controllers/AddCustomerController.java
@@ -1,4 +1,33 @@
 package dhurd.c195.clientdb.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AddCustomerController {
+    public Label addCustLbl;
+    public Label addCustIDLbl;
+    public Label addCustNameLbl;
+    public Label addCustPhoneLbl;
+    public Label addCustAddressLbl;
+    public Label addCustCountryLbl;
+    public Label addCustStateLbl;
+    public Label addCustPostalLbl;
+    public TextField addCustIDTxt;
+    public TextField addCustNameTxt;
+    public TextField addCustPhoneTxt;
+    public TextField addCustAddressTxt;
+    public ComboBox addCustCountryBox;
+    public ComboBox addCustStateBox;
+    public Button addCustSaveBtn;
+    public Button addCustCancelBtn;
+    public TextField addCustPostalTxt;
+
+    public void addCustSave(ActionEvent actionEvent) {
+    }
+
+    public void addCustCancel(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/dhurd/c195/clientdb/controllers/UpdateCustomerController.java
+++ src/main/java/dhurd/c195/clientdb/controllers/UpdateCustomerController.java
@@ -1,4 +1,33 @@
 package dhurd.c195.clientdb.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class UpdateCustomerController {
+    public Label upCustLbl;
+    public Label upCustIDLbl;
+    public Label upCustNameLbl;
+    public Label upCustPhoneLbl;
+    public Label upCustAddressLbl;
+    public Label upCustCountryLbl;
+    public Label upCustStateLbl;
+    public Label upCustPostalLbl;
+    public TextField upCustIDTxt;
+    public TextField upCustNameTxt;
+    public TextField upCustPhoneTxt;
+    public TextField upCustPostalTxt;
+    public TextField upCustAddressTxt;
+    public ComboBox upCustCountryBox;
+    public ComboBox upCustStateBox;
+    public Button upCustSaveBtn;
+    public Button upCustCancelBtn;
+
+    public void updateCustSave(ActionEvent actionEvent) {
+    }
+
+    public void updateCustCancel(ActionEvent actionEvent) {
+    }
 }
