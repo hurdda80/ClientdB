a49b08030eea6b73551c55890c38ba77f613dced
Dean H <dean.hurd80@yahoo.com>
2024-01-18 15:30:20 -0600

    Created "Update Appointment" view and controller shell

---
+++ src/main/java/dhurd/c195/clientdb/controllers/UpdateAppointmentController.java
@@ -0,0 +1,40 @@
+package dhurd.c195.clientdb.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class UpdateAppointmentController {
+    public Label upApptLbl;
+    public Label upApptIDLbl;
+    public Label upApptTitleLbl;
+    public Label upApptDescLbl;
+    public Label upApptLocLbl;
+    public Label upApptContactLbl;
+    public Label upApptStartDateLbl;
+    public Label upApptStartTimeLbl;
+    public TextField upApptIDTxt;
+    public TextField upApptTitleTxt;
+    public TextField upApptDescTxt;
+    public TextField upApptLocTxt;
+    public DatePicker upApptStartDatePicker;
+    public ComboBox upApptStartTimeBox;
+    public Label upApptEndDateLbl;
+    public Label upApptEndTimeLbl;
+    public Label upApptCustIDLbl;
+    public Label upApptUserIDLbl;
+    public DatePicker upApptEndDatePicker;
+    public ComboBox upApptEndTimeBox;
+    public ComboBox upApptCustIDBox;
+    public ComboBox upApptUserIDBox;
+    public ComboBox upApptContactBox;
+    public Label upApptTypeLbl;
+    public TextField upApptTypeTxt;
+    public Button upApptSaveBtn;
+    public Button upApptCancelBtn;
+
+    public void upApptSave(ActionEvent actionEvent) {
+    }
+
+    public void upApptCancel(ActionEvent actionEvent) {
+    }
+}
